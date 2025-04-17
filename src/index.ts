29d282e55604e30273123eb7903dfab60dabc00d
Aotokitsuruya <contact@aotoki.me>
2025-04-17 21:56:28 +0900

    refactor: Simplify imports and remove unused dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
 import "reflect-metadata";
 
-import { LanguageModelV1 } from "ai";
-import { env } from "cloudflare:workers";
 import { Hono } from "hono";
 import { Telegraf } from "telegraf";
 import { container } from "tsyringe";
 
-import "bot/telegram";
-import { AssistantModel } from "./container";
+import "./bot/telegram";
 import { route as RegisterRoute } from "./controller/RegisterController";
 import { route as RootRoute } from "./controller/RootController";
-import { KvConversationRepository } from "./repository/KvConversationRepository";
 
 const app = new Hono();
-const model = container.resolve<LanguageModelV1>(AssistantModel);
-const highModel = container.resolve<LanguageModelV1>(AssistantModel);
-const repository = new KvConversationRepository(env.KV);
 const bot = container.resolve(Telegraf);
 
 app.route("/", RootRoute);
