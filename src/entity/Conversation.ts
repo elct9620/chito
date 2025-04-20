de14d73101d5afa1915f9067e7afbb400e4cab1d
Aotokitsuruya <contact@aotoki.me>
2025-04-20 21:20:02 +0900

    feat: Add messages array and addMessage method to Conversation class

--- src/entity/Conversation.ts
+++ src/entity/Conversation.ts
@@ -1,3 +1,5 @@
+import { Message } from "./Message";
+
 export enum ConversationProvider {
 	Telegram = "tg",
 }
@@ -8,6 +10,7 @@ export enum ConversationType {
 
 export class Conversation {
 	private _type: ConversationType = ConversationType.Private;
+	private _messages: Message[] = [];
 
 	constructor(
 		public readonly conversationId: string,
@@ -18,7 +21,15 @@ export class Conversation {
 		return `${this.provider}-${this.conversationId}`;
 	}
 
+	public get messages(): Message[] {
+		return [...this._messages];
+	}
+
 	public get type(): ConversationType {
 		return this._type;
+	}
+
+	public addMessage(message: Message): void {
+		this._messages = [...this._messages, message];
 	}
 }
